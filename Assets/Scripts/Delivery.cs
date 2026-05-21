eb3658d1ad0013813ba3d42e3280bd823992ec46
Snehodipto Das <dsnehodipto@gmail.com>
2026-05-21 13:46:36 +0530

    File name and dirs edited

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,6 +4,7 @@ public class Delivery : MonoBehaviour {
     bool hasPackage;
     
     void OnTriggerEnter2D(Collider2D collision) {
+        
         if (collision.CompareTag("FoodItem")) {
             Debug.Log("📦Package picked up");
             hasPackage = true;
