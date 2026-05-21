fc640816a3e6f4916ceb9a36fe12ec22d824ca2a
Snehodipto Das <dsnehodipto@gmail.com>
2026-05-21 13:35:17 +0530

    Scenes and environments added

---
+++ Assets/Scripts/Delivery.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Delivery : MonoBehaviour {
+    bool hasPackage;
+    
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("FoodItem")) {
+            Debug.Log("📦Package picked up");
+            hasPackage = true;
+        }
+        if (collision.CompareTag("Customer") && hasPackage) {
+            Debug.Log("Order delivered🚚");        
+            hasPackage = false;    
+        }
+
+    }
+
+}
