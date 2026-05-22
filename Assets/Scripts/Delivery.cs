e9b159ae5e264a2e8eb4daf721d9210c7dacec96
Snehodipto Das <dsnehodipto@gmail.com>
2026-05-23 00:11:07 +0530

    Added UI texts, speed-ups & other stuffs; temporarily finishing

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
 
+[RequireComponent(typeof(ParticleSystem))]
+
 public class Delivery : MonoBehaviour {
     bool hasPackage;
+    [SerializeField] float delay = 0.4f;
     
     void OnTriggerEnter2D(Collider2D collision) {
         
-        if (collision.CompareTag("FoodItem")) {
+        if (collision.CompareTag("FoodItem") && !hasPackage) {
             Debug.Log("📦Package picked up");
             hasPackage = true;
+            GetComponent<ParticleSystem>().Play();
+            Destroy(collision.gameObject, delay);
         }
         if (collision.CompareTag("Customer") && hasPackage) {
             Debug.Log("Order delivered🚚");        
             hasPackage = false;    
+            GetComponent<ParticleSystem>().Stop();
         }
 
     }
--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,11 +1,40 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
-public class Driver : MonoBehaviour {
+public class Driver : MonoBehaviour
+{
 
-    [SerializeField] float moveSpeed = 1f, steerSpeed = 2f;
+    [SerializeField] float currentSpeed = 4.5f;
+    [SerializeField] float steerSpeed = 70f;
+    [SerializeField] float boostSpeed = 10f;
+    [SerializeField] float regularSpeed = 4.5f;
+    [SerializeField] TMP_Text boostText;
 
-    void Update() {
+
+    void Start()
+    {
+        boostText.gameObject.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Boost"))
+        {
+            currentSpeed = boostSpeed;
+            boostText.gameObject.SetActive(true);
+            Debug.Log("Speed boosted up!⚡");
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        currentSpeed = regularSpeed;
+        boostText.gameObject.SetActive(false);
+        Debug.Log("Oh oh..You're hit!😥");
+    }
+
+    void Update()
+    {
         float steer = 0f, move = 0f;
 
         // Move forward 🔼   
@@ -18,7 +47,7 @@ public class Driver : MonoBehaviour {
         if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed) steer = -1f;
 
         float steerAmount = steer * steerSpeed * Time.deltaTime;
-        float moveAmount = move * moveSpeed * Time.deltaTime;
+        float moveAmount = move * currentSpeed * Time.deltaTime;
         transform.Rotate(0, 0, steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
